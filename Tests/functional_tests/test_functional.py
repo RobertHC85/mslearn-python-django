abb1dcd43265146a6327b415d84662a60e7cf11c
Robert Huayta Cortez <46381414+RobertHC85@users.noreply.github.com>
2024-04-22 00:02:07 -0500

    Update test_functional.py

--- Tests/functional_tests/test_functional.py
+++ Tests/functional_tests/test_functional.py
@@ -13,7 +13,6 @@ class FunctionalTests(unittest.TestCase):
 		options.add_argument('--headless')
 		options.add_argument('--no-sandbox')
 		options.add_argument('--disable-dev-shm-usage')
-		#self.driver = webdriver.Chrome(os.path.join(os.environ["CHROMEWEBDRIVER"], 'chromedriver'), options=options)
 		self.driver = webdriver.Chrome(os.path.join(os.environ["CHROMEWEBDRIVER"], 'chromedriver'), options)
 
 
